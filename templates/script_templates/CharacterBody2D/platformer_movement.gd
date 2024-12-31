9fedd8e582ee74844724f3cf24b6868c2852a61e
SMSourov <smsourov.bd@outlook.com>
2025-01-01 02:47:09 +0600

    Update platformer_movement.gd

--- templates/script_templates/CharacterBody2D/platformer_movement.gd
+++ templates/script_templates/CharacterBody2D/platformer_movement.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-# meta-name: Platformer Character Movement
-# meta-description: Predefined script for 2D platformer character
+# meta-name: Platformer Character Template
+# meta-description: Predefined function template script for 2D platformer character.
 # meta-default: true
 # meta-space-indent: 4
 
 func _ready():
+	pass
+
+
+func _physics_process(delta: float) -> void:
 	pass
