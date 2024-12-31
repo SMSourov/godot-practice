3868d82473939546b675efa1d9c279c84bcb7035
SMSourov <smsourov.bd@outlook.com>
2025-01-01 02:29:20 +0600

    create template

---
+++ templates/script_templates/CharacterBody2D/platformer_movement.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+# meta-name: Platformer Character Movement
+# meta-description: Predefined script for 2D platformer character
+# meta-default: true
+# meta-space-indent: 4
+
+func _ready():
+	pass
