71d7a8a64a77da65f1e2b6a04789c1b3bd2b0fe8
SMSourov <smsourov.bd@outlook.com>
2025-01-01 02:52:51 +0600

    character script practice

---
+++ chronbot-practice/script_templates/CharacterBody2D/platformer_movement.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+# meta-name: Platformer Character Template
+# meta-description: Predefined function template script for 2D platformer character.
+# meta-default: true
+# meta-space-indent: 4
+
+func _ready():
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
