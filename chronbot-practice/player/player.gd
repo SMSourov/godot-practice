44ae3e98089842b1fd4d4c76506cb0cf997d2186
SMSourov <smsourov.bd@outlook.com>
2025-01-02 20:32:36 +0600

    key input and player animation

--- chronbot-practice/player/player.gd
+++ chronbot-practice/player/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY = 1000
+const SPEED = 300
 
 enum State { Idle, Run }
 
@@ -13,7 +15,9 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	player_falling(delta)
 	player_idle(delta)
+	player_run()
 	move_and_slide()
+	player_animation()
 
 
 func player_falling(delta: float) -> void:
@@ -24,3 +28,27 @@ func player_falling(delta: float) -> void:
 func player_idle(delta: float) -> void:
 	if is_on_floor():
 		current_state = State.Idle
+
+
+func player_run() -> void:
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	if direction != 0:
+		current_state = State.Run
+
+
+func player_animation():
+	if current_state == State.Idle:
+		animated_sprite_2d.play("idle")
+	elif current_state == State.Run:
+		animated_sprite_2d.play("run")
