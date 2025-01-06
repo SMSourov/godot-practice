880b0fb93f2140063efdf97844a6eb99415bd2ec
SMSourov <smsourov.bd@outlook.com>
2025-01-06 23:23:13 +0600

    player jump state and jump animation

--- chronbot-practice/player/player.gd
+++ chronbot-practice/player/player.gd
@@ -3,23 +3,25 @@ extends CharacterBody2D
 
 const GRAVITY = 1000
 const SPEED = 300
+const JUMP = -300
+const JUMP_HORIZONTAL = 100
 
-enum State { Idle, Run }
+enum State { Idle, Run, Jump }
 
 var current_state: State
 
 func _ready():
 	current_state = State.Idle
 
-func print_state(state: State) -> void:
-	print("State: ", State.keys()[current_state])
 
 func _physics_process(delta: float) -> void:
 	player_falling(delta)
-	player_idle(delta)
+	player_idle()
 	player_run()
+	player_jump(delta)
 	move_and_slide()
 	player_animation()
+	print("State: ", State.keys()[current_state])
 
 
 func player_falling(delta: float) -> void:
@@ -27,13 +29,16 @@ func player_falling(delta: float) -> void:
 		velocity.y += GRAVITY * delta
 
 
-func player_idle(delta: float) -> void:
+func player_idle() -> void:
 	if is_on_floor():
 		current_state = State.Idle
-		print_state(current_state)
+
 
 
 func player_run() -> void:
+	if !is_on_floor():
+		return
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	if direction:
@@ -48,7 +53,16 @@ func player_run() -> void:
 	
 	if direction != 0:
 		current_state = State.Run
-		print_state(current_state)
+
+
+func player_jump(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		velocity.y += JUMP
+		current_state = State.Jump
+	
+	if !is_on_floor() and current_state == State.Jump:
+		var direction = Input.get_axis("move_left", "move_right")
+		velocity.x += direction * JUMP_HORIZONTAL * delta
 
 
 func player_animation():
@@ -56,3 +70,5 @@ func player_animation():
 		animated_sprite_2d.play("idle")
 	elif current_state == State.Run:
 		animated_sprite_2d.play("run")
+	elif  current_state == State.Jump:
+		animated_sprite_2d.play("jump")
